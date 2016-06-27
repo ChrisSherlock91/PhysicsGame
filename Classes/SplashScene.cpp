b98aaaef0d40a2f74c1f5bed10c2c9e4849bcb51
Chris Sherlock <csherlock91@gmail.com>
2016-06-27 18:20:00 +0100

    Added splash scene file

---
+++ Classes/SplashScene.cpp
@@ -0,0 +1,9 @@
+//
+//  SplashScene.cpp
+//  PhysicsGame
+//
+//  Created by Chris Sherlock on 27/06/2016.
+//
+//
+
+#include "SplashScene.h"
---
+++ Classes/SplashScene.h
@@ -0,0 +1,14 @@
+//
+//  SplashScene.h
+//  PhysicsGame
+//
+//  Created by Chris Sherlock on 27/06/2016.
+//
+//
+
+#ifndef SplashScene_h
+#define SplashScene_h
+
+#include <stdio.h>
+
+#endif /* SplashScene_h */
